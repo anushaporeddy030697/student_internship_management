acca84ae82924b814ab0a2be1100996cef655455
shashank546962 <98129286+shashank546962@users.noreply.github.com>
2022-10-14 19:22:20 -0500

    added admin.js file
    database storeage file for admin registration page.

---
+++ src/models/admins.js
@@ -0,0 +1,103 @@
+const mongoose = require("mongoose")
+const validator=require("validator")
+const bcrypt=require("bcryptjs")
+const jwt=require("jsonwebtoken")
+
+
+const adminSchema = mongoose.Schema({
+    firstname:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    lastname:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        unique: true,
+        required:true,
+        trim:true,
+        lowercase:true,
+        
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true,
+            
+    },
+    age:{
+        type:Number,
+        default:0,
+      
+    },
+    gender:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    phone:{
+        type:String,
+        required:true,
+      
+    },
+    tokens:[
+       {
+           token:{
+               type:String,
+               required:true
+           }
+       }
+    ]
+})
+
+//userdef function for hiding private data
+adminSchema.methods.toJSON = function(){
+    const user = this
+    const userObj = user.toObject()
+    delete userObj.password
+    delete userObj.tokens
+    return userObj
+} 
+
+//userdef function for gen auth token
+adminSchema.methods.genAuthToken = async function(){
+    const user=this
+    const token = jwt.sign({_id:user._id.toString()},"thisisseceret",{ expiresIn:"7 days"})
+    user.tokens=user.tokens.concat({token})
+    await user.save()
+    return token
+
+}
+
+//userdef function for authentication
+adminSchema.statics.findByCredentials = async (email,password) => {
+    const user = await admin.findOne({ email })   
+    if(!user){
+        throw new Error("unable to login")
+    }
+    const isMatched = await bcrypt.compare(password,user.password)
+    if(!isMatched){
+        throw new Error("Unable to login")
+    }
+    return user
+}
+
+//using mongoose middleware for hashing passwords
+adminSchema.pre("save",async function (next) {
+    const user =this
+    
+   if(user.isModified('password')){
+       user.password=await bcrypt.hash(user.password,8)
+
+   }
+    next()
+})
+
+//creating a company model
+const admin = mongoose.model('admin',adminSchema)
+
+module.exports=admin
