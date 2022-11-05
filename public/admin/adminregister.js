7c55bef670e6110c7ec57a76e0b1d42c3a922c6b
Chennu venkata shashankar <98129286+shashank546962@users.noreply.github.com>
2022-11-04 19:29:33 -0500

    adminregister.js file is added

---
+++ public/admin/adminregister.js
@@ -0,0 +1,52 @@
+const $adminRegForm=document.getElementById("admin-register")
+
+function verifyPassword(password1,password2) {  
+    if(password1!==password2){
+        document.getElementById("message").innerHTML = "password and confirm password should be same";
+        return false
+    }
+    return true
+   
+  }  
+
+
+$adminRegForm.addEventListener('submit',async (e)=>{
+    e.preventDefault()
+    console.log("data validated")
+    const $firstname=document.getElementById("first-name").value
+    const $lastname=document.getElementById("last-name").value
+    const $email=document.getElementById("email").value
+    const $password=document.getElementById("password").value
+    const $confirmPassword=document.getElementById("password2").value
+    const $gender= document.getElementById("gender").value
+    const $age=document.getElementById("age").value
+    const $phone=document.getElementById("phone").value
+
+    if(verifyPassword($password,$confirmPassword)){
+        const result = await fetch('/admin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                firstname:$firstname,
+                lastname:$lastname,
+                email:$email,
+                password:$password,
+                age:$age,
+                gender:$gender,
+                phone:$phone
+            })
+        }).then((res) => res.json())
+        console.log(result)
+        if (!result.error) {
+            localStorage.setItem('token', result.token)
+            localStorage.setItem('name',result.user.firstname)
+            location.href="/admin/adminLogin.html"
+            alert("success")
+        } else {
+            alert(result.error)
+        }
+    }
+})
+
