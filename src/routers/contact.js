a9b8701beecf88c08cc39df267a6724a70a78fc5
manikantaarumilli <98128314+manikantaarumilli@users.noreply.github.com>
2023-02-03 10:41:15 -0600

    implemented contact.js
    we added the functionality of contact page and the message was delivered successfully to admin

--- src/routers/contact.js
+++ src/routers/contact.js
@@ -1,17 +1,55 @@
-const contact=require("../models/contacts")
-const express=require("express")
+const $contactForm=document.getElementById("contact-send-form")
 
-const router = new express.Router()
-
-
-router.post("/contact",async (req,res)=>{
-    const user = new contact(req.body)
-    try{
-     await user.save() 
-     res.status(201).send({success:"success"})
-    }catch(e){
-     res.status(400).send({error:"error"})
+    console.log("contact info sent")
+    const $name=document.getElementById("name").value
+    const $email=document.getElementById("email").value
+    const $message= document.getElementById("message").value ;
+    $message = $name + "wants to contact and his message was";
+   data = $message + data;
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: $contactForm.method,
+        body: $message + data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your submission!";
+          $contactForm.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
     }
-})
-
-module.exports = router
+    $contactForm.addEventListener("submit", handleSubmit)
+   /* const result = await fetch('/contact', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name:$name,
+            email:$email,
+            message:$message
+        })
+    }).then((res) => res.json())
+    console.log(result,result)
+    if (!result.error) {
+        $contactForm.reset()
+        alert("success")
+    } else {
+        alert("unuble to submit")
+    } */
