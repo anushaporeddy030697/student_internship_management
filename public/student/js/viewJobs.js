26e1a94e6c3264e6ad8e6828a63386f3146c76c5
Chennu venkata shashankar <98129286+shashank546962@users.noreply.github.com>
2023-02-03 20:25:51 -0600

    Added updated viewjobs.js file
    Backend code for search bar functionality

--- public/student/js/viewJobs.js
+++ public/student/js/viewJobs.js
@@ -1,11 +1,60 @@
 const $applybtn=document.querySelector('.apply-btn')
 const token=localStorage.getItem('token')
 const jobbody=document.getElementById('job-body')
+const search=document.getElementById('searchbox')
+const $searchbutton=document.querySelector('.searchbutton')
+
 let usersContainer = document.getElementById("jobs");
 
 // $applybtn.addEventListener('click',(e)=>{
 //     location.href='/student/applypage.html'
 // })
+$searchbutton.addEventListener('click',async (e)=>{
+  const searchterm = search.value
+  //console.log(searchterm);
+  var result = await fetch('/jobs', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization':'Bearer '+token
+    },
+    body: JSON.stringify({ 
+      
+    })
+  }).then((res) => res.json())
+  //console.log(result);
+  const filterTitleFunction = (job)=>{
+    return( job.title.includes(searchterm) ||job.yoe.toString().includes(searchterm)  ||job.companyname.includes(searchterm) ||job.worktype.includes(searchterm))
+  }
+  const filteredResult = result.filter(filterTitleFunction)
+  //console.log(filteredResult);
+  const mappedUsers = filteredResult.map((job, index) => {
+    return `<div class="job">
+    <h1>Job Description</h1>
+    <p>Company Name: ${job.companyname} </p>
+    <p>Job Title: ${job.title}</p>
+    <p>Experience level: ${job.yoe} years</p>
+    <p>
+      Job Responsibilities:${job.requirements}
+    </p>
+    <p>Work Type: ${job.worktype} </p>
+    <p>Employee type: ${job.emptype} </p>
+    <p>Employee benifits: ${job.empbenefits} </p>
+    <button class="btn"><i class="fa-solid fa-download fa-1x"></i><a innerHtml="download About company" download="About company.pdf" href="data:application/octet-stream;base64,${job.aboutcompany.companyData.toString('base64')}"> Download About Company</a></button><br />
+      <button class="apply-btn btn"  onclick="window.location.href='applypage.html?jobid=${job._id}'" type="submit" value="Submit" > Apply </button>    
+  </div>`;
+  });
+
+  if(mappedUsers.length>0){
+    usersContainer.innerHTML = mappedUsers
+}
+  else{
+    alert("no matching results") 
+    //    usersContainer.innerHTML = "<h1>There are no pending applications to review, Please comeback later!</h1>"
+      
+       location.href="/student/viewjobs.html"
+  }
+})
 
 window.onload=async()=>{
     console.log("onload") 
