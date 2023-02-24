f6d8e444059cd732c5c0ac90118afdd398b4dce2
manikantaarumilli <98128314+manikantaarumilli@users.noreply.github.com>
2023-02-24 11:08:37 -0600

    Update mangoose.js
    We try to connect to the database (mongodb)

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://127.0.0.1:27017/student-internship-api")
+
+//mongoose.connect('mongodb+srv://xd666:naveen123@cluster.gbcmjjt.mongodb.net/?retryWrites=true&w=majority'
+//);
 
 
 
+ mongoose.connect('mongodb://127.0.0.1:27017/student-internship-api'
+);
+
+
+
+ 
+
+//mongodb+srv://user:user@cluster0.st75j00.mongodb.net/?retryWrites=true&w=majority
